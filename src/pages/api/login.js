24e91c402b847eb8687ea31acdba994912eff4e7
draysongz <dharmiegra4@gmail.com>
2024-01-21 18:56:08 +0100

    ddd

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,15 +1,16 @@
 // pages/api/login.js
 import { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 
 const clientId = '738539361562109';
+const clientSecret = '8158044eafa5566582a749267606fc19';
 const redirectUri = 'https://know-phi.vercel.app/api/callback';
 
-const loginHandler = (req, res) => {
+const loginHandler = async (req, res) => {
   try {
-    // Redirect users to Facebook login page
-    const loginUrl = `https://www.facebook.com/v18.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=ads_management`;
-    res.writeHead(302, { Location: loginUrl });
-    res.end();
+    // Step 1: Redirect users to Facebook login page
+    const loginUrl = `https://www.facebook.com/v12.0/dialog/oauth?client_id=${clientId}&redirect_uri=${redirectUri}&scope=ads_management`;
+    res.redirect(loginUrl);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
