691122f378a099d1c9860626c3c87c6f315e0497
draysongz <dharmiegra4@gmail.com>
2024-01-21 18:42:15 +0100

    done

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,23 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    window.fbAsyncInit = function() {
+      FB.init({
+        appId: 'your-app-id',
+        autoLogAppEvents: true,
+        xfbml: true,
+        version: 'v18.0',
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  }, []);
   return <Component {...pageProps} />;
 }
