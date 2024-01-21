081b2b4a621c3d3894a47612fe62b0105197de0e
draysongz <dharmiegra4@gmail.com>
2024-01-21 20:05:49 +0100

    ff

--- src/pages/api/ads.js
+++ src/pages/api/ads.js
@@ -11,20 +11,20 @@ const getAdsHandler = async (req, res) => {
     if (!adAccountId || !accessToken) {
       return res.status(400).json({ error: 'Missing ad account ID or access token' });
     }else{
-        return res.status(200).json('everythin has been received from you')
+        // // Fetch all ad groups (ads) for the given ad account
+    const apiUrl = `${FACEBOOK_GRAPH_API_BASE_URL}/${adAccountId}/adgroups?fields=id,name,status&access_token=${accessToken}`;
+    const response = await axios.get(apiUrl);
+
+    if (response.data) {
+      const ads = response.data;
+      console.log('Ads:', ads);
+      res.status(200).json(ads);
+    } else {
+      res.status(500).json({ error: 'Failed to fetch ads' });
+    }
     }
 
-    // // Fetch all ad groups (ads) for the given ad account
-    // const apiUrl = `${FACEBOOK_GRAPH_API_BASE_URL}/${adAccountId}/adgroups?fields=id,name,status&access_token=${accessToken}`;
-    // const response = await axios.get(apiUrl);
-
-    // if (response.data && response.data.data) {
-    //   const ads = response.data.data;
-    //   console.log('Ads:', ads);
-    //   res.status(200).json(ads);
-    // } else {
-    //   res.status(500).json({ error: 'Failed to fetch ads' });
-    // }
+  
   } catch (error) {
     console.error('Error fetching ads:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
