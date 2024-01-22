370856a6bb12d005cdf34c4b8578a005ce18ee8c
draysongz <dharmiegra4@gmail.com>
2024-01-22 05:58:48 +0100

    lets o

--- src/pages/api/ads.js
+++ src/pages/api/ads.js
@@ -1,27 +1,27 @@
 // pages/api/ads.js
-import axios from 'axios';
-
-export default async function handler(req, res) {
-  try {
-    const { query: { adId, accessToken } } = req;
-
-    if (!adId || !accessToken) {
-      return res.status(400).json({ error: 'Missing ad ID or access token' });
+async function handler(adId, accessToken) {
+    try {
+      if (!adId || !accessToken) {
+        console.log('params missing');
+        return; // Added return statement to exit the function when params are missing
+      }
+  
+      const fields = 'insights{impressions}';
+      const apiUrl = `https://graph.facebook.com/v18.0/${adId}?fields=${fields}&access_token=${accessToken}`;
+  
+      const response = await fetch(apiUrl);
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(response)
+        const adMetrics = data.insights;
+        console.log(adMetrics);
+      } else {
+        console.error('Error fetching ad metrics:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching ad metrics:', error);
     }
-
-    const fields = 'insights{impressions}';
-    const apiUrl = `https://graph.facebook.com/v18.0/${adId}?fields=${fields}&access_token=${accessToken}`;
-
-    const response = await axios.get(apiUrl);
-
-    if (response.data && response.data.insights) {
-      const adMetrics = response.data.insights;
-      res.status(200).json(adMetrics);
-    } else {
-      res.status(500).json({ error: 'Failed to fetch ad metrics' });
-    }
-  } catch (error) {
-    console.error('Error fetching ad metrics:', error.message);
-    res.status(500).json({ error: 'Internal Server Error' });
   }
-}
+  
+  handler("act_784150678869646", "EAAKfsphMbf0BO29UApuFYBTGe84blF1IwZAvTNrVtr2HymhPkUHKffxpU0mjFpwcngcsIQaZAwKHhcK7blJPxcwl6uFt6T66LYv6JZBZBElzZB0dBE0zbUNYMXHZBxo3R1cpp8c5Kksjq6kSbGYsLrrt060W39wzymNPgdABSS5SiOZCyMzyZAsZA6khGbjorW2JAPXaSDTZAQNECwNNfmYojT7D3GeErFYXzVVjOFZCUsvHsRuksKXHpwxuBTQO3Ogg3qljQ9b2leUtz5j")
